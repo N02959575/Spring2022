3d9ea3fd9e7e6df8573dab36d67ac819b73a09f5
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-03-03 14:03:33 -0500

    updated files to match

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import Nav from './components/nav.vue';
+import Nav from './components/Nav.vue';
 import Messages from './pages/Messages.vue';
-
+    
 </script>
 
 <template>
-  <Nav/>
-  <div class="container">
-    <section class="section">
-       <h1 class="title">
-    Praying for Ukraine
-  </h1>
-  <Messages/>
-    </section>
-  </div>
- 
+    <Nav />
+
+    <div class="container">
+        <section class="section">
+            <h1 class="title">
+                Praying Ukraine - Праздники в Украине
+            </h1>
+            <Messages />            
+        </section>
+    </div>
+
+    
 </template>
 
 <style>
-
 </style>
