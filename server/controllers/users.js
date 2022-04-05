b8225190cd70ad8b3bbc1648a5245b958136523b
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-04-05 10:25:18 -0400

    used tokens for user authorization

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,32 +1,39 @@
-
+//controller takes java script file and exposes as http
 const express = require('express');
 const app = express.Router();
+const { requireAuth } = require('../models/auth');
 
 const userModel = require('../models/user')
 
 const CREATED_STATUS = 201;
 
 app
-    .get('/',(req,res) => {
+    .get('/', requireAuth, (req,res) => {
         res.send(userModel.list);
     })
     .get('/:id', (req,res) =>{
         const user = userModel.get(req.params.id);
         res.send(user);
     })
-    .post('/', (req,res,next) => {////////////////////////////////////////
+    .post('/', (req,res,next) => {
         userModel.create(req.body)
         .then(user => {
             res.status(CREATED_STATUS).send(user);
         }).catch(next);
     })
-    .delete('/:id', (req,res) => {
+    .delete('/:id', requireAuth, (req,res) => {
         const user = userModel.remove(req.params.id);
         res.send({success: true, errors: [], data: user});
     })
     .patch('/:id', (req,res) => {
         const user = userModel.update(req.params.id, req.body);
         res.send({success: true, errors: [], data: user});
+    })
+    .post('/login', (req,res,next) => {
+        userModel.login(req.body.email, req.body.password)
+        .then(user => {
+            res.send(user);
+        }).catch(next);
     })
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@
 require('dotenv').config();//load enviroment variables
 
 const express = require('express')
+const userModel = require('./models/user')//////////
 const usersController = require('./controllers/users');
 const postsController = require('./controllers/posts');
+const { requireAuth } = require('./models/auth');
 
 
 const app = express()
@@ -15,12 +17,34 @@ app
   .use('/', express.static(__dirname + '/public/'))
 
   .use(express.json())
+  .use((req, res, next) => {///////////////////////
+    const auth = req.headers.authorization;
+    if(auth){
+      const token = auth.split(' ')[1];
+      userModel.fromToken(token)
+        .then(user => {
+          req.user = user;
+          next();
+        })
+        .catch(next)
+    }
+    else{
+      next();
+    }
+  })
 
   .get('/api/', (req, res) => {
   res.send('You are at the root of the API. For the best class ever - ' + process.env.BEST_CLASS_EVER);
   })
   .use('/api/users', usersController)
-  .use('/api/posts', postsController)
+  .use('/api/posts', requireAuth, postsController)////////
+
+  .use((err, req, res, next) => {/////////////
+    console.error(err);
+    
+    res.status(err.statusCode || 500)
+      .send({ errors: [err.message ?? 'Internal server error'] });
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening http://localhost:${port}`)
