0cba1eb0e6dacf55a3ff39ecbe0b18439948fb7c
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-04-11 14:53:52 -0400

    fully crud in the db for users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,21 +8,23 @@ const userModel = require('../models/user')
 const CREATED_STATUS = 201;
 
 app
-    .get('/', requireAuth, (req,res,next) => {///////////////////
+    .get('/', requireAuth, (req,res,next) => {
         userModel.getlist()
         .then(users => {
             res.status(200).json(users);
         }).catch(next)
-
-        //res.send(userModel.list);
     })
-    .get('/:id', (req,res,next) =>{/////////////////////////////
+    .get('/handle/:handle', (req,res,next) =>{
+        userModel.getByHandle(req.params.handle)
+        .then(user => {
+            res.status(200).json(user);
+        }).catch(next)
+    })
+    .get('/:id', (req,res,next) =>{
         userModel.get(req.params.id)
         .then(user => {
             res.status(200).json(user);
         }).catch(next)
-        //const user = userModel.get(req.params.id);
-        //res.send(user);
     })
     .post('/', (req,res,next) => {
         userModel.create(req.body)
@@ -30,13 +32,11 @@ app
             res.status(CREATED_STATUS).send(user);
         }).catch(next);
     })
-    .delete('/:id', requireAuth, (req,res) => {/////////////////////////////
+    .delete('/:id', requireAuth, (req,res) => {
         userModel.remove(req.params.id)
         .then(user => {
             res.send({success: true, errors: [], data: x.insertedIds});
         }).catch(next);
-        //const user = userModel.remove(req.params.id);
-        //res.send({success: true, errors: [], data: user});
     })
     .patch('/:id', (req, res, next) => {
 
@@ -51,7 +51,7 @@ app
             res.send(user);
         }).catch(next);
     })
-    .post('/seed', (req,res,next) => {///////////////////
+    .post('/seed', (req,res,next) => {
         userModel.seed()
         .then(x => {
             res.send({success: true, errors: [], data: x.insertedIds});
--- server/models/user.js
+++ server/models/user.js
@@ -37,6 +37,17 @@ const list = [{
 
 async function get(id){
     const user = await collection.findOne({ _id: new ObjectId(id) });
+    if(!user){
+        throw { statusCode: 404, message: 'User not found' };
+    }
+    return { ...user, password: undefined }
+}
+
+async function getByHandle(handle){
+    const user = await collection.findOne({ handle });
+    if(!user){
+        throw { statusCode: 404, message: 'User not found' };
+    }
     return { ...user, password: undefined }
 }
 
@@ -46,23 +57,25 @@ async function remove(id){
     return { ...user.value, password: undefined };
 }
 
-async function update(id, newUser){
-    const index = list.findIndex(user => user.id === parseInt(id));
-    const oldUser = list[index];
+async function update(id, newUser){////////////////////////
 
     if(newUser.password){
         newUser.password = await bcrypt.hash(newUser.password, 10);
     }
 
-    newUser = list[index] = { ...oldUser, ...newUser };
-
+    newUser = await collection.findOneAndUpdate(
+        { _id: new ObjectId(id) }, 
+        { $set: newUser },
+        { returnDocument: 'after' }
+    );
+    
     console.log(newUser);
     
     return { ...newUser, password: undefined};
 }
 
-async function login(email, password){
-    const user = list.find(user => user.email === email);
+async function login(email, password){//////////////////////////
+    const user = await collection.findOne({ email });
     if(!user){
         throw { statusCode: 404, message: 'User not found' };
     }
@@ -98,15 +111,18 @@ function seed(){
 module.exports = {
     collection,
     seed,
-    async create(user) {
+    getByHandle,
+    async create(user) {//////////////////////////////
         user.id = ++highestId;
+        if(!user.handle){
+            throw { statusCode: 400, message: 'Handle is required' };
+        }
         //the plus converts string to number
         user.password = await bcrypt.hash(user.password, +process.env.SALT_ROUNDS);
         console.log(user);
 
-        throw {message: 'This is a test error'};
-
-        list.push(user);
+        const result = await collection.insertOne(user);
+        user = await get(result.insertedId);
         return {...user, password: undefined};
     },
     remove,
