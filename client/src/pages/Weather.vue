3e40e716548210eb8d8566e2d9cd81812f18107b
N02959575 <menam3@hawkmail.newpaltz.edu>
2022-05-02 15:07:58 -0400

    just id

--- client/src/pages/Weather.vue
+++ client/src/pages/Weather.vue
@@ -1,12 +1,29 @@
+<script lang="ts">
+  declare var mapIsReady: Promise<void>;
+</script>
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
     const weather = reactive({ data: {} as any});
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=New Paltz&appid=${import.meta.env.VITE_OPENWEATHER_KEY}`)
-            .then(x=>x.json())
-            .then(x=>{
-            console.log(x);
-            weather.data = Object.entries(x.main);
-
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${import.meta.env.VITE_OPENWEATHER_KEY}`)
+    //const weather = await fetch('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0')
+        .then(x=>x.json())
+        .then(x=>{
+                console.log(x);
+                weather.data = Object.entries(x.main);
+        });
+    const location = ref<any>({})
+    mapIsReady.then(() => {
+        
+        
+        var request = {
+            query: 'Museum of Contemporary Art',
+            fields: ['name', 'geometry'],
+        };
+        var service = new google.maps.places.PlacesService(document.createElement('div'));
+        service.findPlaceFromQuery(request, function(results, status) {
+            console.log(results);
+            location.value = results![0];
+        });
     });
 </script>
 
@@ -29,9 +46,13 @@ import { reactive } from 'vue';
                 </tr>
             </tbody>
         </table>
+
+        {{ location.name }}
+        {{ location.geometry.location.lat() }}
+        {{ location.geometry.location.lng() }}
     </div>
 </template>
 
 
 <style scoped>
-</style> 
+</style>
